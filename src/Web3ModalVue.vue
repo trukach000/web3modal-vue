fa4f60b2d0922458c3f12979396d815d26709f8e
Pavel <trukach000@gmail.com>
2022-03-28 22:00:07 +0300

    small fix

--- src/Web3ModalVue.vue
+++ src/Web3ModalVue.vue
@@ -141,6 +141,7 @@ export default {
       this.show = !this.show;
     },
     onError(error) {
+      this.providerController.clearCachedProvider();
       if (this.show) {
         this._toggleModal();
       }

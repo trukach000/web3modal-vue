d10626a993f5e6991ea2e828f97b7a116f35436c
Pavel <trukach000@gmail.com>
2022-03-29 12:53:00 +0300

    fixed metamask detection bump version

--- src/providers/connectors/metamask.js
+++ src/providers/connectors/metamask.js
@@ -1,10 +1,17 @@
 const ConnectToMetamask = async () => {
     let provider = null;
     if (typeof window.ethereum !== 'undefined') {
-        if ( window.ethereum.providers && window.ethereum.providers.length > 0){
+        if ( window.ethereum.providers && window.ethereum.providers.length > 1){
           provider = window.ethereum.providers.find((provider) => provider.isMetaMask);
+          if (typeof provider === 'undefined') {
+            throw new Error("No metamask provider found");
+          }
         } else {
-          throw new Error("No metamask provider found");
+          if (window.ethereum.isMetaMask) {
+            provider = window.ethereum;
+          } else {
+            throw new Error("No metamask provider found");
+          }
         }
         try {
             await provider.request({method: 'eth_requestAccounts'})
